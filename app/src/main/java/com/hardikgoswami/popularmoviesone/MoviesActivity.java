38f0606f4cf1e5c1c09f67023f60faf0f8c2c63a
geniushkg <geniushkg@gmail.com>
2016-05-02 13:44:29 +0530

    spinner implementation done , implement onclick gridview

--- app/src/main/java/com/hardikgoswami/popularmoviesone/MoviesActivity.java
+++ app/src/main/java/com/hardikgoswami/popularmoviesone/MoviesActivity.java
@@ -2,9 +2,14 @@ package com.hardikgoswami.popularmoviesone;
 
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.hardikgoswami.popularmoviesone.fragments.MoviesGridFragment;
 
@@ -14,6 +19,7 @@ public class MoviesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         ft.add(R.id.fragment_container_movie_activity, new MoviesGridFragment());
--- app/src/main/java/com/hardikgoswami/popularmoviesone/fragments/MoviesGridFragment.java
+++ app/src/main/java/com/hardikgoswami/popularmoviesone/fragments/MoviesGridFragment.java
@@ -3,11 +3,19 @@ package com.hardikgoswami.popularmoviesone.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.hardikgoswami.popularmoviesone.PopularMovieApplication;
 import com.hardikgoswami.popularmoviesone.R;
@@ -24,16 +32,18 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MoviesGridFragment extends Fragment {
-
+    public sortOrder SORT_ORDER = sortOrder.POPULAR;
     GridView gridView;
     private MovieGridAdapter mMovieGridAdapter;
 
     public MoviesGridFragment() {
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -48,25 +58,85 @@ public class MoviesGridFragment extends Fragment {
 
         return rootView;
     }
-
-    private void updateAdapterData() {
-        Call<Popular> callPopularMovies = PopularMovieApplication.getsService().getPopularMovies(PopularMovieApplication.TMDB_API_KEY);
-        callPopularMovies.enqueue(new Callback<Popular>() {
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.activity_menu, menu);
+        MenuItem item = menu.findItem(R.id.spinner);
+        Spinner spinner = (Spinner) MenuItemCompat.getActionView(item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.sort_order, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onResponse(Call<Popular> call, Response<Popular> response) {
-                if (response.isSuccessful()) {
-                    Popular movieObject = response.body();
-                    ArrayList<Movie> movieList = new ArrayList<Movie>(movieObject.getResults());
-                    mMovieGridAdapter.clear();
-                    mMovieGridAdapter.addAll(movieList);
-                    mMovieGridAdapter.notifyDataSetChanged();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (parent.getItemAtPosition(position).toString().equalsIgnoreCase("popular")){
+                    SORT_ORDER = sortOrder.POPULAR;
+                }else {
+                    SORT_ORDER = sortOrder.TOP_RATED;
                 }
+                updateAdapterData();
             }
 
             @Override
-            public void onFailure(Call<Popular> call, Throwable t) {
-                Log.d("TAG", "retrofit failed execute with :"+t.getMessage());
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(getContext(),"nothing selected",Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void updateAdapterData() {
+        switch (SORT_ORDER) {
+            case TOP_RATED: {
+                Call<Popular> callPopularMovies = PopularMovieApplication.getsService().getTopRatedMovies(PopularMovieApplication.TMDB_API_KEY);
+                callPopularMovies.enqueue(new Callback<Popular>() {
+                    @Override
+                    public void onResponse(Call<Popular> call, Response<Popular> response) {
+                        if (response.isSuccessful()) {
+                            Popular movieObject = response.body();
+                            ArrayList<Movie> movieList = new ArrayList<Movie>(movieObject.getResults());
+                            mMovieGridAdapter.clear();
+                            mMovieGridAdapter.addAll(movieList);
+                            mMovieGridAdapter.notifyDataSetChanged();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Popular> call, Throwable t) {
+                        Log.d("TAG", "retrofit failed execute with :" + t.getMessage());
+                    }
+                });
+                break;
+            }
+            case POPULAR: {
+                Call<Popular> callPopularMovies = PopularMovieApplication.getsService().getPopularMovies(PopularMovieApplication.TMDB_API_KEY);
+                callPopularMovies.enqueue(new Callback<Popular>() {
+                    @Override
+                    public void onResponse(Call<Popular> call, Response<Popular> response) {
+                        if (response.isSuccessful()) {
+                            Popular movieObject = response.body();
+                            ArrayList<Movie> movieList = new ArrayList<Movie>(movieObject.getResults());
+                            mMovieGridAdapter.clear();
+                            mMovieGridAdapter.addAll(movieList);
+                            mMovieGridAdapter.notifyDataSetChanged();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Popular> call, Throwable t) {
+                        Log.d("TAG", "retrofit failed execute with :" + t.getMessage());
+                    }
+                });
+                break;
+            }
+            default:
+                Log.d("TAG", "default case , sort order issue");
+
+        }
+    }
+
+    public enum sortOrder {
+        TOP_RATED, POPULAR
     }
 }
