5b5068117cf286c97b80ccf693480fe2df92f92d
geniushkg <geniushkg@gmail.com>
2016-05-05 00:27:08 +0530

    implementation complete

--- app/src/main/java/com/hardikgoswami/popularmoviesone/fragments/MovieDetail.java
+++ app/src/main/java/com/hardikgoswami/popularmoviesone/fragments/MovieDetail.java
@@ -1,29 +1,27 @@
 package com.hardikgoswami.popularmoviesone.fragments;
 
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.hardikgoswami.popularmoviesone.R;
 import com.hardikgoswami.popularmoviesone.model.Movie;
+import com.squareup.picasso.Picasso;
 
-import org.parceler.Parcels;
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MovieDetail extends Fragment {
     Movie model;
-    TextView tvTitle,tvRating,tvDate;
+    TextView tvTitle,tvRating,tvDate,tvOverview;
     ImageView ivPoster;
-    String posterUrl,rating,date,originalTitle;
+    String posterUrl,rating,date,originalTitle,overview;
     public MovieDetail() {
         // Required empty public constructor
     }
@@ -38,6 +36,8 @@ public class MovieDetail extends Fragment {
             rating = model.getVoteAverage() + "/ 10";
             date = model.getReleaseDate();
             originalTitle = model.getOriginalTitle();
+            overview = model.getOverview();
+
         }
 
     }
@@ -54,6 +54,12 @@ public class MovieDetail extends Fragment {
         tvDate = (TextView) rootView.findViewById(R.id.tv_movie_release_date);
         tvDate.setText(date);
         ivPoster = (ImageView) rootView.findViewById(R.id.iv_movie_detail);
+        Picasso.with(getContext())
+                .load(posterUrl)
+                .placeholder(R.drawable.progress_animation)
+                .into(ivPoster);
+        tvOverview = (TextView) rootView.findViewById(R.id.tv_movie_overview);
+        tvOverview.setText(overview);
 
         return rootView;
     }
